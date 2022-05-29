bc963273f8689243a77b61a057f400482b59d586
andychen <z60509561@gmail.com>
2022-05-29 12:13:17 +0800

    init

---
+++ src/test/java/org/cclemon/cclemon/CclemonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cclemon.cclemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CclemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
