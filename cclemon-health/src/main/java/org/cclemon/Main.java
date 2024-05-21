eb864679b21de7c1ea572a79fc2ac4e5bfcf603b
andychen <z60509561@gmail.com>
2024-05-21 15:47:04 +0800

    PG-創建cclemon-base 1.初始化專案

--- cclemon-health/src/main/java/org/cclemon/Main.java
+++ cclemon-health/src/main/java/org/cclemon/Main.java
@@ -1,11 +1,8 @@
 package org.cclemon;
 
-import org.cclemon.base.utils.TestUtils;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println(TestUtils.test());
         System.out.println("Hello world!");
     }
 }
--- src/main/java/org/cclemon/base/utils/TestUtils.java
+++
@@ -1,7 +0,0 @@
-package org.cclemon.base.utils;
-
-public class TestUtils {
-    public static String test() {
-        return "TEST1";
-    }
-}
