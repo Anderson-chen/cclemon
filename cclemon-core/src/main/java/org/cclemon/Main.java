80f74415deabcad97f25465e8911e3dc3cba91f3
andychen <z60509561@gmail.com>
2024-05-22 17:50:07 +0800

    PG-創建cclemon-base 1.初始化專案

--- cclemon-core/src/main/java/org/cclemon/Main.java
+++ cclemon-core/src/main/java/org/cclemon/Main.java
@@ -1,7 +1,13 @@
 package org.cclemon;
 
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println(CollectionUtils.isEmpty(new ArrayList<>()));
         System.out.println("Hello core !");
     }
 }
