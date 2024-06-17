2c095cb7211c3159c22e6f8968f4b54c5991f8ff
andychen <z60509561@gmail.com>
2024-06-17 13:59:17 +0800

    PG-創建cclemon-pratice

---
+++ cclemon-practice/src/main/java/operator/Increment.java
@@ -0,0 +1,11 @@
+package operator;
+
+public class Increment {
+
+    public static int test1() {
+
+
+
+        return 0;
+    }
+}
---
+++ cclemon-practice/src/test/java/operator/IncrementTest.java
@@ -0,0 +1,12 @@
+package operator;
+
+import org.junit.jupiter.api.Test;
+
+class IncrementTest {
+
+    @Test
+    void test1() {
+        Increment.test1();
+
+    }
+}
