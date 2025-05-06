f077402841c39ea485a731496fe37b3ad14ede09
andychen <zz60509561@gmail.com>
2025-05-06 12:58:36 +0800

    feat:新增eventBus專案

---
+++ cclemon-eventBus/src/test/java/org/cclemon/CclemonEventBusApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cclemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CclemonEventBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
