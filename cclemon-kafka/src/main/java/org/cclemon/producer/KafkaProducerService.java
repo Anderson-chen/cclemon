cbcab2291ed550e058b3263498825af1bbdacc5e
1302738-NB\wits <zz60509561@gmail.com>
2025-12-23 17:58:41 +0800

    task:spring boot 4 升級

--- cclemon-kafka/src/main/java/org/cclemon/producer/KafkaProducerService.java
+++ cclemon-kafka/src/main/java/org/cclemon/producer/KafkaProducerService.java
@@ -1,6 +1,7 @@
 package org.cclemon.producer;
 
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.Message;
@@ -10,13 +11,10 @@ import org.springframework.stereotype.Service;
 import java.util.UUID;
 
 @Service
+@AllArgsConstructor
 public class KafkaProducerService {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
-
-    public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
 
     @Value("${kafka.topic}")
     private String topic;
