ffd146b0e0331ac8382bf0cca95deb0b01c4b8a6
1302738-NB\wits <zz60509561@gmail.com>
2026-01-02 17:26:34 +0800

    Revert "task:oauth2相關資料改用redis存取"
    This reverts commit cf9f09d6eaa2c683f94f1f208794f43079535497.

--- cclemon-cache/src/main/java/org/cclemon/service/RedisService.java
+++ cclemon-cache/src/main/java/org/cclemon/service/RedisService.java
@@ -3,8 +3,6 @@ package org.cclemon.service;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.time.Duration;
-
 @Service
 public class RedisService {
 
@@ -14,21 +12,11 @@ public class RedisService {
         this.redisTemplate = redisTemplate;
     }
 
-    // 帶 TTL
-    public void set(String key, String value, Duration ttl) {
-        redisTemplate.opsForValue().set(key, value, ttl);
-    }
-
-    // 不帶 TTL
     public void set(String key, String value) {
         redisTemplate.opsForValue().set(key, value);
     }
 
     public String get(String key) {
         return redisTemplate.opsForValue().get(key);
-    }
-
-    public void delete(String key) {
-        redisTemplate.delete(key);
     }
 }
